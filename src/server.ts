eab1a3df8760763f07ac887a8d926ad468a06f1a
Carlos Daniel <carlosporrasayala@gmail.com>
2024-12-01 01:22:09 -0500

    Conectar la aplicación a la base de datos

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import { productsRouter } from "./router";
+import { db } from "./config/db";
+
+// Connect to the database
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión a la base de datos establecida correctamente.");
+  } catch (error) {
+    console.error("No se pudo conectar a la base de datos:", error);
+  }
+}
+connectDB();
 
 const server = express();
 
 // Routing
 server.use("/api/products", productsRouter);
 
-export default server;
+export { server };
